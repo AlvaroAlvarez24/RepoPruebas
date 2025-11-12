ece80c2901ede6fb125960b4b8b1bde289db7496
aalvarez <alvaro.alvarez@openintl.com>
2025-11-12 15:50:27 -0500

    otro intento de subir dato sensible

--- probando/Program.cs
+++ probando/Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine("Hello, Platzi!");
 
 
 
+string testingConnection = "DefaultConnection": "Server=localhost;Database=TuBaseDeDatos;User Id=tu_usuario;Password=tu_contraseña;TrustServerCertificate=True;"
